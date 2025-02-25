8404c69d5afbbf4a6771fab3602143c43d17f9c4
Serhiy Holiaka <holiaka.s@gmail.com>
2025-02-25 15:40:30 +0200

    add weblate project

---
+++ apps/weblate/src/i18n.js
@@ -0,0 +1,38 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import LastUsed from "locize-lastused";
+
+i18n
+  // i18next-locize-backend
+  // loads translations from your project, saves new keys to it (saveMissing: true)
+  // https://github.com/locize/i18next-locize-backend
+  .use(Backend)
+  // locize-lastused
+  // sets a timestamp of last access on every translation segment on locize
+  // -> safely remove the ones not being touched for weeks/months
+  // https://github.com/locize/locize-lastused
+  .use(LastUsed)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    saveMissing: true,
+    // keySeparator: false,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    react: {
+      bindI18n: "languageChanged editorSaved",
+    },
+  });
+
+export default i18n;
