f8093e277cd99558ae20d402ba00b54d56a8fee6
Serhiy Holiaka <holiaka.s@gmail.com>
2025-03-17 19:26:33 +0200

    teste commit 4

--- apps/locize-http-backend/src/main.tsx
+++ apps/locize-http-backend/src/main.tsx
@@ -7,6 +7,8 @@ import { Suspense } from "react";
 
 import "./i18n";
 
+//TODO: Add a new key to the translation file
+
 const lngs = {
   en: { nativeName: "English" },
   uk: { nativeName: "Ukrainian" },
